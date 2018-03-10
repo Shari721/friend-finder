46add0201c0eea221293d7682e4476f9295c7ecd
Shari Bratanch <sharibratanch@Sharis-MacBook-Air.local>
2018-03-09 22:17:56 -0600

    push

---
+++ app/data/friends.js
@@ -0,0 +1,171 @@
+// ===============================================================================
+// DATA
+// Below data will hold all of the reserved tables.
+// Initially we just set it equal to a "dummy" customer.
+// But you could have it be an empty array as well.
+// ====
+
+var friendsArray = [
+  {
+    "name": "Jim",
+    "photo": "https://www.istockphoto.com/photos/men",
+    "scores": [
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+     ]
+  },
+  {
+	"name": "Bob",
+  "photo": "http://www.ifred.org/men"
+    "scores": [
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+     ]
+  },
+{
+	"name": "Kenny",
+    "photo": "http://www.studiomedicofilippini.it/trattamenti/sessualita-maschile/",
+    "scores": [
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+     ]
+  },
+  {
+	"name": "Jared",
+    "photo": "http://www.suave.com/product/category/661957/men",
+    "scores": [
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+     ]
+  },
+  {
+	"name": "Rebecca",
+    "photo": "http://www.qygjxz.com/women-pictures.html",
+    "scores": [
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+     ]
+  },
+  {
+	"name": "Joanne",
+    "photo": "http://adage.com/article/special-report-women-to-watch/women-watch-roisin-donnelly-procter-gamble-u-k-ireland/235084/",
+    "scores": [
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+     ]
+  },
+  {
+	"name": "Kelly",
+    "photo": "https://pixabay.com/da/attraktivt-smuk-pige-model-person-1869761/",
+    "scores": [
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+     ]
+  },
+  {
+	"name": "Ashley",
+    "photo": "http://distinguishedyw.org/scholarships/",
+    "scores": [
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+     ]
+  },
+  {
+	"name": "Anna",
+    "photo": "https://www.pexels.com/search/women/",
+    "scores": [
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+     ]
+  },
+  {
+	"name": "Coco",
+    "photo": "https://www.inverse.com/article/7497-salon-manager-dead-in-freezing-cryochamber-accident-leaves-a-mystery",
+    "scores": [
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+     ]
+  }];
+
+// Note how we export the array. This makes it accessible to other files using require.
+module.exports = friendsArray;
